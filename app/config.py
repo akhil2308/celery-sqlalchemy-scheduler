c365391fd9ed05931fc4b61c247caaee785e9c95
Ch.Akhil <46097218+akhil2308@users.noreply.github.com>
2025-03-20 23:18:10 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
+    SCHEDULER_SYNC_EVERY = int(os.getenv("SCHEDULER_SYNC_EVERY", 60))
+    
     REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
     REDIS_PORT = os.getenv("REDIS_PORT", "6379")
     REDIS_DB = os.getenv("REDIS_DB", "0")
